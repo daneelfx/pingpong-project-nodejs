a1df7ddefeb250a84b27ca158e8f4f7452627885
Daniel Felipe <34217277+daneelfs@users.noreply.github.com>
2019-08-11 16:15:23 -0500

    email sending was added

---
+++ src/emails/account.js
@@ -0,0 +1,26 @@
+const sgMail = require("@sendgrid/mail")
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: "PingPongSimijaca@gmail.com",
+        subject: "Gracias por registrarte a PingPong Simijaca",
+        text: `Bienvenido ${name}. Ahora eres parte de PingPong Simijaca`
+    })
+}
+
+const sendCancelationEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: "PingPongSimijaca@gmail.com",
+        subject: "Es una lástima ver que te vas :c",
+        text: "${name}, ¿Por qué te vas?"
+    })
+}
+
+module.exports = {
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require("./db/mongoose")
 const userRouter = require("./routers/user")
 const tournamentRouter = require("./routers/tournament")
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
